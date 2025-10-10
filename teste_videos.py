a7b4ae4d874a1bc90dccc9bb6184f7d690ea8e45
Larissa <srta.raimee@gmail.com>
2025-10-10 12:58:06 -0300

    more videos

--- teste_videos.py
+++ teste_videos.py
@@ -4,7 +4,7 @@ caminho_do_modelo = 'runs/detect/kfold_kfold_dataset_stratified_10_10folds_fold_
 MODEL = YOLO(caminho_do_modelo)
 
 # Defina o caminho do vídeo que você quer processar
-caminho_do_video = 'arquivos_teste/IMG_6010.MOV'
+caminho_do_video = 'arquivos_teste/IMG_6015.MOV'
 
 # Faça a predição com save=True
 # O YOLO vai cuidar de tudo e salvar o resultado
